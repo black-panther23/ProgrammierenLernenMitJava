ce8ade7df3d2aaa45348c1177e81e98719cffba2
black-panther23 <blackpanther23@gmail.com>
2026-09-02 18:36:54 +0200

    upload first exercise task

---
+++ kapitel01/Kreisberechnung.java
@@ -0,0 +1,17 @@
+package kapitel01;
+
+// Kreisberechnung: Für einen Kreis mit dem Radius 5 cm werden der Umfang und der Flächeninhalt berechnet 
+
+
+public class Kreisberechnung {
+    public static void main(String[] args) {
+        double radius, umfang, inhalt;
+        radius = 5.0;
+        umfang = 2.0 * 3.1415926 * radius;
+        inhalt = 3.1415926 * radius * radius;
+        System.out.print("Umfang: ");
+        System.out.println(umfang);
+        System.out.print("Flaeche: ");
+        System.out.println(inhalt);
+    }
+}
