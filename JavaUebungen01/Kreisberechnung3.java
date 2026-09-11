6355a7c8b429fb1f7b3790a8a64bb02e9447072c
black-panther23 <blackpanther23@gmail.com>
2026-09-11 15:39:44 +0200

    Create new File

---
+++ JavaUebungen01/Kreisberechnung3.java
@@ -0,0 +1,20 @@
+package JavaUebungen01;
+
+/* Kreisberechnung: Für einen Kreis werden der Umfang un der Flächeninhalt berechnet.
+ * Der Radius wird beim Programmstart als erster Parameter und die Einheit wird als zweiter Parameter übergeben.
+ */
+
+public class Kreisberechnung3 {
+    public static void main(String[] args) {
+        var einheit = args[1];
+        var radius = Double.parseDouble(args[0]);
+        var umfang = 2.0 * 3.1415926 * radius;
+        var flaeche = 3.1415926 * radius * radius;
+        System.out.print("Umfang: ");
+        System.out.print(umfang + " " + einheit);
+        System.out.println(" " + einheit);
+        System.out.print("Fläche: ");
+        System.out.print(flaeche);
+        System.out.println(" " + einheit + '\u00b2');
+    }
+}
