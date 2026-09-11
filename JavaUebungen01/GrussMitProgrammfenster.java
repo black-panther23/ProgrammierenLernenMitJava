7c2109a2bc751b18e454019bc99326293a7adaf5
black-panther23 <blackpanther23@gmail.com>
2026-09-11 16:13:38 +0200

    Create new file

---
+++ JavaUebungen01/GrussMitProgrammfenster.java
@@ -0,0 +1,39 @@
+package JavaUebungen01;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+
+public class GrussMitProgrammfenster extends JFrame {
+    public GrussMitProgrammfenster() {
+        super("Hallo");
+
+
+        var icon = new ImageIcon("JavaUebungen01/Panda.jpg");
+        var label1 = new JLabel("Viel Erfolg beim", JLabel.CENTER);
+        var label2 = new JLabel("Programmieren mit Java!", JLabel.CENTER);
+        var label3 = new JLabel(icon);
+
+        var schrift = new Font("SansSerif", Font.BOLD, 24);
+        label1.setFont(schrift);
+        label1.setForeground(Color.black);
+        label2.setFont(schrift);
+        label2.setForeground(Color.black);
+
+        var c = getContentPane();
+        c.setLayout(new FlowLayout());
+        c.setBackground(Color.white);
+        c.add(label1);
+        c.add(label2);
+        c.add(label3);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300,350);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GrussMitProgrammfenster();
+    }
+}
