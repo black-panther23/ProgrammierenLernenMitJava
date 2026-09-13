2d643106988ca17c6987594d7ce60d8c78c9278a
black-panther23 <blackpanther23@gmail.com>
2026-09-13 15:58:02 +0200

    Create new file

---
+++ JavaUebungen02/Variablen2.java
@@ -0,0 +1,27 @@
+package JavaUebungen02;
+
+public class Variablen2 {
+    public static void main(String[] args) {
+        
+        //Initialisierungen
+        char z1 = 'a';
+        char z2 = 'b';
+        char z3 = 'A';
+        char z4 = 169;
+        char z5 = 190;
+
+        //Ausgaben
+        System.out.print("z1: ");
+        System.out.println(z1);
+        System.out.print("z2: ");
+        System.out.println(z2); 
+        System.out.print("z3: ");
+        System.out.println(z3); 
+        System.out.print("z4: ");
+        System.out.println(z4); 
+        System.out.print("z5: ");
+        System.out.println(z5); 
+
+    }
+}
+        
